3ca147a74cb413f5d00c63f393fa7b4cddbc7ae7
Sebastian Billing <sebastian@Sebastians-MacBook-Air.local>
2021-05-06 19:11:33 +0200

    d

--- src/index.js
+++ src/index.js
@@ -19,6 +19,6 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`körs på http://localhost:${port}`)
   })
 
